7a87c257584a4a885d4ee4e246f7e14056b414f7
Pavel Halko <pavelhalko@Aliaksandrs-MacBook-Pro.local>
2022-01-27 16:22:54 -0500

    first commit

---
+++ app/src/main/java/com/example/roman/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.roman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.start_button);
+        text = (TextView)findViewById(R.id.title_text);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Hello");
+            }
+        });
+    }
+
+}
