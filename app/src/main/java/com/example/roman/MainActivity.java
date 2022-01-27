e012e82a0ee2f0b8be8a83cc535c309d5a0f0d60
Pavel Halko <pavelhalko@Aliaksandrs-MacBook-Pro.local>
2022-01-27 17:27:00 -0500

    input to text

--- app/src/main/java/com/example/roman/MainActivity.java
+++ app/src/main/java/com/example/roman/MainActivity.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
     TextView text;
+    EditText textTwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button)findViewById(R.id.start_button);
         text = (TextView)findViewById(R.id.title_text);
+        textTwo = (EditText) findViewById(R.id.name);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(textTwo.getText());
+            }
+        });
 //        button.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
