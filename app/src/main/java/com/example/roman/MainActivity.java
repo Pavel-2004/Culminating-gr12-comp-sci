fde0d3fbb2373e0282e6ea48bc2a65530a8c09b4
Pavel Halko <pavelhalko@Aliaksandrs-MacBook-Pro.local>
2022-01-28 14:03:12 -0500

    layout change

--- app/src/main/java/com/example/roman/MainActivity.java
+++ app/src/main/java/com/example/roman/MainActivity.java
@@ -21,9 +21,107 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button)findViewById(R.id.start_button);
+
+
+        Button buttonC = (Button)findViewById(R.id.C);
+        Button buttonD = (Button)findViewById(R.id.D);
+        Button buttonM = (Button)findViewById(R.id.M);
+        Button buttonL = (Button)findViewById(R.id.L);
+        Button buttonX = (Button)findViewById(R.id.X);
+        Button buttonV = (Button)findViewById(R.id.V);
+        Button buttonI = (Button)findViewById(R.id.V);
+        Button buttonPlus = (Button)findViewById(R.id.plus);
+        Button buttonMinus = (Button)findViewById(R.id.minus);
+
+
         text = (TextView)findViewById(R.id.title_text);
-        textTwo = (EditText) findViewById(R.id.name);
+
+
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add C
+                text.setText(String.valueOf(text.getText()) + "C");
+            }
+        });
+
+
+        buttonD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add D
+                text.setText(String.valueOf(text.getText()) + "D");
+            }
+        });
+
+
+        buttonM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add M
+                text.setText(String.valueOf(text.getText()) + "M");
+            }
+        });
+
+        buttonL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add L
+                text.setText(String.valueOf(text.getText()) + "L");
+            }
+        });
+
+        buttonX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add X
+                text.setText(String.valueOf(text.getText()) + "X");
+            }
+        });
+
+        buttonV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add V
+
+                text.setText(String.valueOf(text.getText()) + "V");
+            }
+        });
+
+        buttonI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(String.valueOf(text.getText()) + "I");
+            }
+        });
+
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add plus
+                text.setText(String.valueOf(text.getText()) + "+");
+            }
+        });
+
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add minus
+                text.setText(String.valueOf(text.getText()) + "-");
+            }
+        });
+        /*
+
+
+
+        button = (Button)findViewById(R.id.start_button);
+
+         */
+        text = (TextView)findViewById(R.id.title_text);
+        //textTwo = (EditText) findViewById(R.id.name);
+
+        /*
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
         });
         Calculations addN = new Calculations("V");
         Log.i("answer", String.valueOf(addN.add("V+V")));
-
+        */
     }
 
 }
