48b90ead863a3af6a10aeb143e50e33f377b072e
halkopavel@gmail.com <pavelhalko@Aliaksandrs-MacBook-Pro.local>
2022-01-29 15:08:20 -0500

    scroll feature

--- app/src/main/java/com/example/roman/MainActivity2.java
+++ app/src/main/java/com/example/roman/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,6 +33,9 @@ public class MainActivity2 extends AppCompatActivity {
 
         TextView current = (TextView)findViewById(R.id.title_text);
         TextView answer = (TextView)findViewById(R.id.answer);
+
+
+        answer.setMovementMethod(new ScrollingMovementMethod());
 
 
         Calculations addN = new Calculations("");
